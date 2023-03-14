0caf6ffaf69c48785199154cbc6d681348eadd75
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2023-03-14 12:24:19 +0300

    palindrome test refactoring

--- js-algorithms-and-data-structures/palindrome-checker/__tests__/palindrome.test.ts
+++ js-algorithms-and-data-structures/palindrome-checker/__tests__/palindrome.test.ts
@@ -1,17 +1,19 @@
 import {isPalindrome} from '../palindrome';
 
-test('plain palindromes', () => {
-	expect(isPalindrome('eye')).toBeTruthy();
-	expect(isPalindrome('racecar')).toBeTruthy();
-});
+describe('isPalindrome', () => {
+	it('Correctly checks plain palindromes', () => {
+		expect(isPalindrome('eye')).toBeTruthy();
+		expect(isPalindrome('racecar')).toBeTruthy();
+	});
 
-test('camelCase palindromes', () => {
-	expect(isPalindrome('RaceCar')).toBeTruthy();
-});
+	it('Correctly checks camelCase palindromes', () => {
+		expect(isPalindrome('RaceCar')).toBeTruthy();
+	});
 
-test('palindromes with spaces and characters', () => {
-	expect(isPalindrome('race CAR')).toBeTruthy();
-	expect(isPalindrome('2A3*3a2')).toBeTruthy();
-	expect(isPalindrome('2A3 3a2')).toBeTruthy();
-	expect(isPalindrome('2_A3*3#A2')).toBeTruthy();
+	it('Correctly checks palindromes with spaces and characters', () => {
+		expect(isPalindrome('race CAR')).toBeTruthy();
+		expect(isPalindrome('2A3*3a2')).toBeTruthy();
+		expect(isPalindrome('2A3 3a2')).toBeTruthy();
+		expect(isPalindrome('2_A3*3#A2')).toBeTruthy();
+	});
 });
