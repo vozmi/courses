f0059201d4f50b6ddc4a6edb669d184bed54ecf7
mvozbranyuk <mvozbranyuk@data-platform.ru>
2023-03-14 11:52:52 +0300

    plain palindromes check added

--- js-algorithms-and-data-structures/palindrome-checker/palindrome.ts
+++ js-algorithms-and-data-structures/palindrome-checker/palindrome.ts
@@ -1,3 +1,20 @@
+/**
+ * Reverses a string
+ * @param str input string
+ * @returns reversed string
+ */
+function reverseString(str: string) {
+	return str
+		.split('') // Create array of chars
+		.reverse() // Reverse array of chars
+		.join(''); // Join reversed array
+}
+
+/**
+ * Check if string is palindrome
+ * @param str input string
+ * @returns true if string is palindrome
+ */
 export function isPalindrome(str: string): boolean {
-	return true;
+	return str === reverseString(str);
 }
