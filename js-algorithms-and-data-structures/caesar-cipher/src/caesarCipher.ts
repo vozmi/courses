db96f7af1ea092b32f12212a09823207c989d371
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2023-03-29 18:24:09 +0300

    add implementation

--- js-algorithms-and-data-structures/caesar-cipher/src/caesarCipher.ts
+++ js-algorithms-and-data-structures/caesar-cipher/src/caesarCipher.ts
@@ -1,4 +1,34 @@
+type Alphabet = string[];
+
+function decryptCaesarCypher(encrypted: string, indent: number) {
+    const alphabetUpper: Alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const alphabetLower: Alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    let result = '';
+
+    for (const c of encrypted) {
+        let isUpper = false;
+
+        if (c.toUpperCase() === c) {
+            isUpper = true;
+        }
+
+        const alphabet: Alphabet = isUpper ? alphabetUpper : alphabetLower;
+
+        const encryptedCharPos = alphabet.findIndex(x => x === c);
+
+        if (encryptedCharPos === -1) {
+            result += c; // Not character (i.e. number, symbol, e.t.c.)
+        } else {
+            const decryptedCharPos = (encryptedCharPos + indent) % alphabet.length;
+
+            result += alphabet[decryptedCharPos];
+        }
+    }
+
+    return result;
+}
 
 export function rot13(encrypted: string): string {
-    throw 'Not implemented!';
+    return decryptCaesarCypher(encrypted, 13);
 }
