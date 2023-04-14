7f33fbdeca7d91e7ba3e7b4ab859a0fbacc68b1a
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2023-04-14 17:54:39 +0300

    remove "|"

--- js-algorithms-and-data-structures/phone-number-validator/src/telephoneCheck.ts
+++ js-algorithms-and-data-structures/phone-number-validator/src/telephoneCheck.ts
@@ -5,13 +5,13 @@
  */
 export function telephoneCheck(phoneNumber: string): boolean {
     // [1]{1}? - one or zero digits (1 - US phone code)
-    // [\s|-]? - " " or "-"
+    // [\s-]? - " " or "-"
     // [(]? - one or zero "("
     // [0-9]{3} - three digits
     // [)]? - one or zero ")"
-    // [\s|-]? - " " or "-"
+    // [\s-]? - " " or "-"
     // [0-9]{4} - four digits
-    const regex = /^[1]{1}?[\s|-]?[(]?[0-9]{3}[)]?[\s|-]?[0-9]{3}[\s|-]?[0-9]{4}$/;
+    const regex = /^[1]{1}?[\s-]?[(]?[0-9]{3}[)]?[\s-]?[0-9]{3}[\s-]?[0-9]{4}$/;
 
     return regex.test(phoneNumber);
 }
