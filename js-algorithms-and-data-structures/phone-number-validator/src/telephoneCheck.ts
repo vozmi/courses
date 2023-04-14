bc9b72e4958e52aeb04af916a2fe7c2c3bf78f9b
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2023-04-14 17:34:21 +0300

    add regex (v1)

--- js-algorithms-and-data-structures/phone-number-validator/src/telephoneCheck.ts
+++ js-algorithms-and-data-structures/phone-number-validator/src/telephoneCheck.ts
@@ -3,6 +3,13 @@
  * @param phoneNumber
  * @returns true if the passed string looks like a valid US phone number, false if don't
  */
-export function telephoneCheck(phoneNumber: string): string {
-    throw 'Not implemented!';
+export function telephoneCheck(phoneNumber: string): boolean {
+    // [0-9]{1} - one digit
+    // [\s|-|(]* - " " or "-"
+    // [0-9]{3} - three digits
+    // [\s|-|(]* - " " or "-"
+    // [0-9]{4} - four digits
+    const regex = /[0-9]{1}[\s|-]*[0-9]{3}[\s|-]*[0-9]{3}[\s|-]*[0-9]{4}/;
+
+    return regex.test(phoneNumber);
 }
