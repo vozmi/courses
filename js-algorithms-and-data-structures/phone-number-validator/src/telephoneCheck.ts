0d4312d86078f6410d85927978efeaaafffd487a
mvozbranyuk <mvozbranyuk@data-platform.ru>
2023-04-14 17:43:43 +0300

    regex v3 (from any "*" to one or zero "?")

--- js-algorithms-and-data-structures/phone-number-validator/src/telephoneCheck.ts
+++ js-algorithms-and-data-structures/phone-number-validator/src/telephoneCheck.ts
@@ -5,13 +5,13 @@
  */
 export function telephoneCheck(phoneNumber: string): boolean {
     // [0-9]{1}? - one or zero digits
+    // [\s|-]? - " " or "-"
     // [(]? - one or zero "("
-    // [\s|-]* - " " or "-"
     // [0-9]{3} - three digits
-    // [\s|-]* - " " or "-"
     // [)]? - one or zero ")"
+    // [\s|-]? - " " or "-"
     // [0-9]{4} - four digits
-    const regex = /[0-9]{1}?[(]?[\s|-]*[0-9]{3}[\s|-]*[)]?[0-9]{3}[\s|-]*[0-9]{4}/;
+    const regex = /[0-9]{1}?[\s|-]?[(]?[0-9]{3}[)]?[\s|-]?[0-9]{3}[\s|-]?[0-9]{4}/;
 
     return regex.test(phoneNumber);
 }
