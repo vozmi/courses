08100642bc805abb666107ce53f0a2e46c725ca1
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2023-04-15 01:00:38 +0300

    fix description

--- js-algorithms-and-data-structures/phone-number-validator/src/telephoneCheck.ts
+++ js-algorithms-and-data-structures/phone-number-validator/src/telephoneCheck.ts
@@ -6,9 +6,7 @@
 export function telephoneCheck(phoneNumber: string): boolean {
     // ([1]{1})? - one or zero digits (1 - US phone code)
     // [\s-]? - " " or "-"
-    // [(]? - one or zero "("
-    // [0-9]{3} - three digits
-    // [)]? - one or zero ")"
+    // (\([0-9]{3}\)|[0-9]{3}) - (XXX) or XXX
     // [\s-]? - " " or "-"
     // [0-9]{4} - four digits
     const regex = /^([1]{1})?[\s-]?(\([0-9]{3}\)|[0-9]{3})[\s-]?[0-9]{3}[\s-]?[0-9]{4}$/;
