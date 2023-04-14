318cef02c518c0a95bf027381743ac2c6656f25c
mvozbranyuk <mvozbranyuk@data-platform.ru>
2023-04-15 00:59:08 +0300

    fix parenthesis condition (regex v5)

--- js-algorithms-and-data-structures/phone-number-validator/src/telephoneCheck.ts
+++ js-algorithms-and-data-structures/phone-number-validator/src/telephoneCheck.ts
@@ -4,14 +4,14 @@
  * @returns true if the passed string looks like a valid US phone number, false if don't
  */
 export function telephoneCheck(phoneNumber: string): boolean {
-    // [1]{1}? - one or zero digits (1 - US phone code)
+    // ([1]{1})? - one or zero digits (1 - US phone code)
     // [\s-]? - " " or "-"
     // [(]? - one or zero "("
     // [0-9]{3} - three digits
     // [)]? - one or zero ")"
     // [\s-]? - " " or "-"
     // [0-9]{4} - four digits
-    const regex = /^([1]{1})?[\s-]?[(]?[0-9]{3}[)]?[\s-]?[0-9]{3}[\s-]?[0-9]{4}$/;
+    const regex = /^([1]{1})?[\s-]?(\([0-9]{3}\)|[0-9]{3})[\s-]?[0-9]{3}[\s-]?[0-9]{4}$/;
 
     return regex.test(phoneNumber);
 }
