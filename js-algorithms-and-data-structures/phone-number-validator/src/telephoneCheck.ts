4200ec43fc4d74c6d8607dcabd37b0e00fdfdeae
mvozbranyuk <mvozbranyuk@data-platform.ru>
2023-04-14 13:13:26 +0300

    add phone number validator project (not implemented)

---
+++ js-algorithms-and-data-structures/phone-number-validator/src/telephoneCheck.ts
@@ -0,0 +1,8 @@
+/**
+ * Converts number from
+ * @param phoneNumber
+ * @returns true if the passed string looks like a valid US phone number, false if don't
+ */
+export function telephoneCheck(phoneNumber: string): string {
+    throw 'Not implemented!';
+}
