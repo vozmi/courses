b8ce9df06f7474eb9213b54ab4dc5c98a3e7a523
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2023-04-15 00:56:43 +0300

    fix first digit optionality

--- js-algorithms-and-data-structures/phone-number-validator/src/telephoneCheck.ts
+++ js-algorithms-and-data-structures/phone-number-validator/src/telephoneCheck.ts
@@ -11,7 +11,7 @@ export function telephoneCheck(phoneNumber: string): boolean {
     // [)]? - one or zero ")"
     // [\s-]? - " " or "-"
     // [0-9]{4} - four digits
-    const regex = /^[1]{1}?[\s-]?[(]?[0-9]{3}[)]?[\s-]?[0-9]{3}[\s-]?[0-9]{4}$/;
+    const regex = /^([1]{1})?[\s-]?[(]?[0-9]{3}[)]?[\s-]?[0-9]{3}[\s-]?[0-9]{4}$/;
 
     return regex.test(phoneNumber);
 }
