e89e2e2ca1bcfda073b14bdd5f5db3d6a1d45545
mvozbranyuk <mvozbranyuk@data-platform.ru>
2023-04-15 12:29:07 +0300

    Add more types

--- js-algorithms-and-data-structures/cash-register/src/checkCashRegister.ts
+++ js-algorithms-and-data-structures/cash-register/src/checkCashRegister.ts
@@ -1,4 +1,31 @@
-type CashInDrawer = Array<[string, number]>;
+type CurrencyUnit =
+    | 'PENNY'
+    | 'NICKEL'
+    | 'DIME'
+    | 'QUARTER'
+    | 'ONE'
+    | 'FIVE'
+    | 'TEN'
+    | 'TWENTY'
+    | 'ONE HUNDRED';
+
+type CashInDrawer = Array<[CurrencyUnit, number]>;
+
+/* eslint-disable @typescript-eslint/naming-convention */
+type CashDictionary = Record<CurrencyUnit, number>;
+
+const cashDictionary: CashDictionary = {
+    PENNY: 0.01,
+    NICKEL: 0.05,
+    DIME: 0.1,
+    QUARTER: 0.25,
+    ONE: 1,
+    FIVE: 5,
+    TEN: 10,
+    TWENTY: 20,
+    'ONE HUNDRED': 100,
+};
+/* eslint-enable @typescript-eslint/naming-convention */
 
 type CashRegisterStatus = 'OPEN' | 'CLOSED' | 'INSUFFICIENT_FUNDS';
 
