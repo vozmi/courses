ec5c8529f0418e48e4e628bda7632f1db17ecf6e
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2023-04-15 11:53:58 +0300

    add cash register project

---
+++ js-algorithms-and-data-structures/cash-register/src/checkCashRegister.ts
@@ -0,0 +1,9 @@
+/**
+ * Checks cash register (ability to make purchase and how many change should be returned)
+ * @param price purchase price
+ * @param cash payment
+ * @param cid cash-in-drawer
+ */
+export function checkCashRegister(price: number, cash: number, cid: Array<[string, number]>) {
+    throw 'Not implemented!';
+}
