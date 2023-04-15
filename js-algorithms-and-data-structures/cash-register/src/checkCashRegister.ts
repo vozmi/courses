4affa0de7de2cd5d40ae96d85766a6ded06648a6
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2023-04-15 12:00:07 +0300

    add types

--- js-algorithms-and-data-structures/cash-register/src/checkCashRegister.ts
+++ js-algorithms-and-data-structures/cash-register/src/checkCashRegister.ts
@@ -1,9 +1,18 @@
+type CashInDrawer = Array<[string, number]>;
+
+type CashRegisterStatus = 'OPEN' | 'CLOSED' | 'INSUFFICIENT_FUNDS';
+
+type CashRegisterState = {
+    status: CashRegisterStatus;
+    change: CashInDrawer;
+};
+
 /**
  * Checks cash register (ability to make purchase and how many change should be returned)
  * @param price purchase price
  * @param cash payment
  * @param cid cash-in-drawer
  */
-export function checkCashRegister(price: number, cash: number, cid: Array<[string, number]>) {
+export function checkCashRegister(price: number, cash: number, cid: CashInDrawer): CashRegisterState {
     throw 'Not implemented!';
 }
