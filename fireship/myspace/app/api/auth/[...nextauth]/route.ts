c011b42d5e599270381ff1f3e8ccf42e9f26d408
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2024-08-30 17:24:37 +0400

    feat: add AuthStore adapter

--- fireship/myspace/app/api/auth/[...nextauth]/route.ts
+++ fireship/myspace/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,11 @@
+import { prisma } from '@/shared/db/lib';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import type { NextAuthOptions } from "next-auth";
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
