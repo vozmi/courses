f9e1060a206b62839a77d5a7eb5c74b32030eb3f
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2024-09-12 17:58:50 +0400

    feat: add global error handler and loader for users page

---
+++ fireship/myspace/app/error.tsx
@@ -0,0 +1,20 @@
+'use client';
+import {useEffect} from "react";
+
+export default function Error({error, reset}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Something went wrong</h2>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <button onClick={reset}>Try again</button>
+    </div>
+  );
+}
