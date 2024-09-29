4bd99239341ae4ebd00f0300f7d5cd020e023493
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2024-09-29 23:52:26 +0400

    add cors middleware

--- fireship/cloud-functions/functions/src/app.ts
+++ fireship/cloud-functions/functions/src/app.ts
@@ -1,7 +1,11 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
+import * as cors from "cors";
 
 const app = express();
+
+app.use(cors({origin: true}));
+
 app.get("/express", (req, res) => {
   res.send("Hello from Express!");
 });
