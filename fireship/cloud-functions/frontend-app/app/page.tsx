1590edc8cd87fc0e4933e880167809c53c15523d
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2024-09-30 03:04:13 +0400

    fix comments and styles

--- fireship/cloud-functions/frontend-app/app/page.tsx
+++ fireship/cloud-functions/frontend-app/app/page.tsx
@@ -1,17 +1,18 @@
 import {SignIn} from "@/features/sign-in";
 
 export default function Home() {
-    return (
-        <div>
-            <header style={{
-                display: "flex",
-                justifyContent: "space-evenly",
-                padding: "16px 32px",
-                borderBottom: "1px solid #eee"
-            }}>
-                <h1>Firebase Learning App</h1>
-                <SignIn />
-            </header>
-        </div>
-    );
+  return (
+    <div>
+      <header style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "0 32px",
+        borderBottom: "1px solid #eee"
+      }}>
+        <h1>Firebase Learning App</h1>
+        <SignIn/>
+      </header>
+    </div>
+  );
 }
