dbf646124827337c329023791bb6ea6753bda7ee
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2024-09-30 02:39:45 +0400

    fix functions/auth eslint errors

--- fireship/cloud-functions/functions/src/auth.ts
+++ fireship/cloud-functions/functions/src/auth.ts
@@ -3,22 +3,23 @@ import * as admin from "firebase-admin";
 
 const db = admin.firestore();
 
-export const createUserRecord = functions.auth.user().onCreate(async (user, context) => {
-  const { email, uid } = user;
+export const createUserRecord = functions.auth.user().onCreate(
+  async (user, context) => {
+    const {email, uid} = user;
 
-  if (!email) {
-    throw new functions.https.HttpsError(
-      "failed-precondition",
-      "The function must be called " +
-        "while authenticated with an email identity."
-    );
+    if (!email) {
+      throw new functions.https.HttpsError(
+        "invalid-argument",
+        "Email address is required.",
+      );
+    }
+
+    const userRef = db.collection("users").doc(uid);
+
+    return userRef.set({
+      uid,
+      email,
+      createdAt: context.timestamp,
+    });
   }
-
-  const userRef = db.collection("users").doc(uid);
-
-  return userRef.set({
-    uid,
-    email,
-    createdAt: context.timestamp,
-  });
-});
+);
