9a9427ff9fb80a18e9213d7347f9b19a675645a3
Maksim Vozbraniuk <mvozbranuk@gmail.com>
2024-09-30 00:11:01 +0400

    add createUserRecord function

---
+++ fireship/cloud-functions/functions/src/auth.ts
@@ -0,0 +1,24 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+const db = admin.firestore();
+
+export const createUserRecord = functions.auth.user().onCreate(async (user) => {
+  const { email, uid } = user;
+
+  if (!email) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called " +
+        "while authenticated with an email identity."
+    );
+  }
+
+  const userRef = db.collection("users").doc(uid);
+
+  return userRef.set({
+    uid,
+    email,
+    createdAt: admin.firestore.FieldValue.serverTimestamp(),
+  });
+});
--- fireship/cloud-functions/functions/src/index.ts
+++ fireship/cloud-functions/functions/src/index.ts
@@ -1,24 +1,6 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
-export const helloWorld = onRequest((request, response) => {
-  const name = request.query.name;
-
-  logger.info("Hello logs!", {structuredData: true});
-
-  response.send(`Hello ${name || "World"}!`);
-});
-
 export {api} from "./app";
+export {createUserRecord} from "./auth";
